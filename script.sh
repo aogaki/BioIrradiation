b9ff025f418d5dadc9132e249757acc37b1f14a0
aogaki <sohichiroh.aogaki@eli-np.ro>
2015-11-12 09:07:54 +0200

    Removing useless attenuator data sets

--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@ kD="300"
 
 outDir="Data/$kMat$kT""D$kD"
 mkdir -p $outDir
+rm -f $outDir/*
 rm -f tmp.root
 rm -f tmp.mac
 
@@ -17,10 +18,12 @@ echo "/BI/Primary/Z -$kD" >> tmp.mac
 echo "/run/beamOn $kEvents" >> tmp.mac
 
 rm -f attT.dat
-ln -sf AttData/attAl1000Dose1.0.dat attT.dat
+ln -sf AttData/att"$kMat$kT".dat attT.dat
 
 for ((i=1;i<=1;i++)); do
     ./BI -m tmp.mac
     root -l -q run.cpp+O
     mv tmp.root $outDir/$i.root
 done
+
+hadd -f "$kMat$kT""D$kD.root" $outDir/*.root
