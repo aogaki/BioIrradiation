33758016b265af6c58a87b9c4196b7d21c2975bd
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2016-03-02 16:30:13 +0200

    Making attenuator file for old beam

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ for((i=$1;i<=$2;i++))
 do
     suffix=`printf %04d $i`
     ./BI -m test.mac --grid --oldbeam --tile
-    hadd -f resultT160.$suffix.root result_t*.root
+    hadd -f result.$suffix.root result_t*.root
 done
