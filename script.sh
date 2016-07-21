fc7306c8adcc3b8baa5d757cd9baf314b99a8d89
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2016-07-21 14:22:29 +0300

    script.sh is making tar script now.

--- script.sh
+++ script.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-#kEvents="10000000"
-kEvents="10000"
-kMat="Al"
-kT="1000"
-kD="300"
+for((i=0;i<=20;i++))
+do
+    rm -f circle.att
+    
+    for((j=0;j<70;j++))
+    do
+	echo $((i * 500)) >> circle.att
+    done
 
-outDir="Data/$kMat$kT""D$kD"
-mkdir -p $outDir
-rm -f $outDir/*
-rm -f tmp.root
-rm -f tmp.mac
-
-echo "/BI/Geometry/WindowMaterial G4_$kMat" > tmp.mac
-echo "/BI/Geometry/WindowThickness $kT" >> tmp.mac
-echo "/BI/Primary/Z -$kD" >> tmp.mac
-echo "/run/beamOn $kEvents" >> tmp.mac
-
-rm -f attT.dat
-ln -sf AttData/att"$kMat$kT".dat attT.dat
-
-for ((i=1;i<=1;i++)); do
-    ./BI -m tmp.mac
-    root -l -q run.cpp+O
-    mv tmp.root $outDir/$i.root
+    tar zcvf bi$i.tgz *
+    mv bi*.tgz ../
+    
 done
-
-hadd -f "$kMat$kT""D$kD.root" $outDir/*.root
